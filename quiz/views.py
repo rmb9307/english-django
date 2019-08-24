4ab38ebdebbc3f845ae9275a04fb63b2f54e98a5
Rita Bulman <ritabulman@outlook.com>
2019-08-23 23:53:09 -0400

    added quiz model

--- quiz/views.py
+++ quiz/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-
-quizzes = [
-    {
-        'part_of_speech': 'noun',
-        'title': 'Nouns',
-        'instructions': 'Please click on the nouns in the following sentences.'
-    },
-    {
-        'part_of_speech': 'verb',
-        'title': 'Verbs',
-        'instructions': 'Please click on the verbs in the following sentences.'
-    },
-    {
-        'part_of_speech': 'adjective',
-        'title': 'Adjectives',
-        'instructions': 'Please click on the adjectives in the following sentences.'
-    }
-]
+from .models import Quiz
 
 def home(request):
     context = {
-        'quizzes': quizzes
+        'quizzes': Quiz.objects.all()
     }
     return render(request, 'quiz/home.html', context)
 
